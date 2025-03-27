4a73c0691fae7e4d85a2ec3e4d890406a402261f
Ryan Dyson Darmawan <dysonryan6@gmail.com>
2025-03-27 17:30:12 +0800

    testing

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   schema: "./src/server/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.NEXT_PUBLIC_DATABASE_URL as string,
+    url: process.env.DATABASE_URL as string,
   },
 });
